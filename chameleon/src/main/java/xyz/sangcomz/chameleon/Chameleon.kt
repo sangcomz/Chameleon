e05503b15f03b4557f5ea9cf69de9ea7f77d586e
sangcomz <sangcomz@naver.com>
2018-02-26 21:25:40 +0900

    progress

--- chameleon/src/main/java/xyz/sangcomz/chameleon/Chameleon.kt
+++ chameleon/src/main/java/xyz/sangcomz/chameleon/Chameleon.kt
@@ -1,21 +1,28 @@
 package xyz.sangcomz.chameleon
 
 import android.content.Context
+import android.graphics.drawable.AnimationDrawable
 import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
+import android.support.v4.widget.TextViewCompat
+import android.support.v7.widget.AppCompatImageView
 import android.support.v7.widget.AppCompatTextView
 import android.support.v7.widget.RecyclerView
 import android.text.TextUtils
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import xyz.sangcomz.stickytimelineview.ext.DP
 
 /**
  * Created by sangcomz on 12/02/2018.
  */
 open class Chameleon(context: Context?, attrs: AttributeSet?) : ConstraintLayout(context, attrs) {
     private var childView: RecyclerView? = null
+    private var stateImageView: AppCompatImageView? = null
+    private var stateTextView: AppCompatTextView? = null
+
     override fun addView(child: View?) {
         checkValid(child)
         super.addView(child)
@@ -37,13 +44,17 @@ open class Chameleon(context: Context?, attrs: AttributeSet?) : ConstraintLayout
     }
 
     private fun addStateView() {
-        val layoutParams = ViewGroup.LayoutParams(100, 100)
-        val textView = TextView(context)
-        textView.text = "abcd"
-        val ImageView = ImageView(context)
-
-        super.addView(textView, layoutParams)
-        super.addView(ImageView, layoutParams)
+        initStateImageViewView()
+        initStateTextView()
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(this)
+        constraintSet.addToVerticalChain(R.id.iv_state,
+                ConstraintLayout.LayoutParams.PARENT_ID,
+                R.id.tv_state)
+        constraintSet.addToVerticalChain(R.id.tv_state,
+                R.id.iv_state,
+                ConstraintLayout.LayoutParams.PARENT_ID)
+        constraintSet.applyTo(this)
     }
 
 
@@ -61,19 +72,45 @@ open class Chameleon(context: Context?, attrs: AttributeSet?) : ConstraintLayout
         addStateView()
     }
 
-    private fun addStateTextView() {
-        val stateTextView = AppCompatTextView(context)
-        stateTextView.id = R.id.tv_state
-        stateTextView.text = "Hallo Chameleon"
-        stateTextView.ellipsize = TextUtils.TruncateAt.END
-        val layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.MATCH_CONSTRAINT)
-        layoutParams.topToBottom = R.id.iv_state
-        layoutParams.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID
+    private fun initStateImageViewView() {
+        val padding = 4.DP(context).toInt()
+        stateImageView = AppCompatImageView(context)
+        stateImageView?.apply {
+            id = R.id.iv_state
+            setBackgroundResource(R.drawable.drawable_progress)
+            setPadding(padding, padding, padding, padding)
+        }
+        val background = stateImageView?.background
+        if (background is AnimationDrawable) {
+            background.start()
+        }
+        val layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT)
         layoutParams.startToStart = ConstraintLayout.LayoutParams.PARENT_ID
         layoutParams.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID
+        layoutParams.verticalChainStyle = ConstraintLayout.LayoutParams.CHAIN_PACKED
+        super.addView(stateImageView, layoutParams)
+    }
 
-
-
+    private fun initStateTextView() {
+        val padding = 4.DP(context).toInt()
+        stateTextView = AppCompatTextView(context)
+        stateTextView?.apply {
+            id = R.id.tv_state
+            text = context.getString(R.string.msg_empty)
+            setPadding(padding * 4, padding, padding * 4, padding)
+            maxLines = 1
+            ellipsize = TextUtils.TruncateAt.END
+            setTextSize(TypedValue.COMPLEX_UNIT_SP, 24f)
+            TextViewCompat.setAutoSizeTextTypeWithDefaults(this,
+                    TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM)
+        }
+        val layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT)
+        layoutParams.startToStart = ConstraintLayout.LayoutParams.PARENT_ID
+        layoutParams.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID
         super.addView(stateTextView, layoutParams)
     }
+
+
 }
